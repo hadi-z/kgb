ebf6c7f692fead5d1cc004808a64726a1723ab2c
Hadi Ahmadi <hadizamanabad@gmail.com>
2022-11-28 22:07:41 +0330

    with '#' will be ignored, and an empty message aborts the commit.

---
+++ 13/js/cd.js
@@ -0,0 +1,9 @@
+
+function ens(){
+     document.getElementById('ent').innerHTML='هیچ گونه انتخاباتی برای کاربر وجود ندارد';
+     document.getElementById('ent').style.border= '1px dashed grey';
+     document.getElementById('ent').style.direction= 'rtl';
+     document.getElementById('ent').style.float= 'right';
+     document.getElementById('ent').style.background= 'rgba(234, 248, 255, 0.35)';
+
+}
